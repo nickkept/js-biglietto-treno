67c32353838dfd32ea85d6639b8784a663f5bf38
nickkept <nickit1899@gmail.com>
2022-10-27 16:20:19 +0200

    ; added

--- js/biglietto.js
+++ js/biglietto.js
@@ -1,14 +1,14 @@
 const numbKm = prompt("Quanti km vuoi percorrere?");
 const age = prompt("Quanti anni hai?");
-const ticketPrice = numbKm * 0.21
+const ticketPrice = numbKm * 0.21;
 
 if (age < 18) {
 
-    const ticketPrice = numbKm * 0.21 * 0.8
-    console.log("Ticket Price Under 18:", ticketPrice.toFixed(2))
+    const ticketPrice = numbKm * 0.21 * 0.8;
+    console.log("Ticket Price Under 18:", ticketPrice.toFixed(2));
 } else if (age > 65) {
-    const ticketPrice = numbKm * 0.21 * 0.6
-    console.log("Ticket Price Over 65:", ticketPrice.toFixed(2))
+    const ticketPrice = numbKm * 0.21 * 0.6;
+    console.log("Ticket Price Over 65:", ticketPrice.toFixed(2));
 } else {
-    console.log("Regular Ticket Price", ticketPrice)
+    console.log("Regular Ticket Price", ticketPrice);
 }
